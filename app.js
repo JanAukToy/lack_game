ee00b32332aacff2c8963c4bc3456c8825cd226b
JanAukToy <56356028+JanAukToy@users.noreply.github.com>
2020-04-18 02:08:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
+
+
 let random = Math.floor(Math.random() * 2) + 1;
 left = 1;
 right = 2;
 count = 0;
 
+
 function push(btnNo) {
+
+  let logt = document.getElementById("log").textContent;
+
+  let log = document.getElementById("log");
+
   if (btnNo === 1) {
     if (random === left) {
       count += 1;
-      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").innerText = count;
+
+      if(count>logt){
+        log.innerHTML=count;
+      }
     }
     else {
       count = 0;
-      document.getElementById("count").innerHTML = count;
-    }
-  }
+      document.getElementById("count").innerText = count;
+    };
+  };
+
   if (btnNo === 2) {
     if (random === right) {
       count += 1;
-      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").innerText = count;
+
+      if(count>logt){
+        log.innerHTML=count;
+      }
     }
     else {
       count = 0;
-      document.getElementById("count").innerHTML = count;
-    }
-  }
+      document.getElementById("count").innerText = count;
+    };
+  };
   random = Math.floor(Math.random() * 2) + 1;
+  return logt;
 };
